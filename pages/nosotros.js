025df56e74aeec2472db5b60e0b4fa9843eb8aed
NicoPicotto <nicopicotto@hotmail.com>
2022-07-17 22:17:31 -0300

    Nosotros

--- pages/nosotros.js
+++ pages/nosotros.js
@@ -1,6 +1,5 @@
 import {
 	Flex,
-	Box,
 	Image,
 	useColorMode,
 	Divider,
@@ -8,8 +7,6 @@ import {
 	Text,
 } from '@chakra-ui/react';
 const nosotros = () => {
-	const { colorMode } = useColorMode();
-
 	return (
 		<Flex alignItems={'center'}>
 			<Flex
@@ -19,14 +16,42 @@ const nosotros = () => {
 				marginRight={-100}
 				borderRightRadius={20}
 			></Flex>
-			<Flex height={'50vh'} marginLeft={-100} boxShadow={"md"} borderRadius={20}>
-				<Image src={'/assets/about.jpg'} borderRadius={20} boxShadow={"md"}/>
+			<Flex
+				height={'50vh'}
+				marginLeft={-100}
+				boxShadow={'md'}
+				borderRadius={20}
+			>
+				<Image src={'/assets/about.jpg'} borderRadius={20} boxShadow={'md'} />
 			</Flex>
-			<Flex height={'50vh'} marginLeft={10} width={"35vw"}flexDir={"column"} ali>
-				<Heading size={"3xl"} color={"brand.primario"}fontFamily={'fonts.open'}>Nosotros</Heading>
-				<Divider orientation='horizontal' marginTop={3} marginBottom={3}/>
+			<Flex
+				height={'50vh'}
+				marginLeft={10}
+				width={'35vw'}
+				flexDir={'column'}
+				ali
+			>
+				<Heading
+					size={'3xl'}
+					color={'brand.primario'}
+					fontFamily={'fonts.open'}
+				>
+					Nosotros
+				</Heading>
+				<Divider orientation='horizontal' marginTop={3} marginBottom={3} />
 				<Text fontFamily={'fonts.open'}>
-				CLEANERGY RENOVABLES S.A. nace en 2016 con la misión de crear economías circulares, ayudando a generar valor a municipios, industrias y la comunidad mediante sistemas de generación de energía a partir de residuos orgánicos. Creemos que la incorporación de nuevas tecnologías a los emprendimientos productivos, especialmente en la industria agroalimentaria, cambiará la manera de hacer negocios en Argentina y la región, adoptando prácticas más eficientes, y a la vez más sustentables. Es por eso que trabajamos junto a productores agropecuarios e industriales para aprovechar un recurso valioso, que hoy está desaprovechado, generando negocios más rentables, con menor riesgo, y amigables con el medio ambiente y la comunidad en la que trabaja.
+					CLEANERGY RENOVABLES S.A. nace en 2016 con la misión de crear
+					economías circulares, ayudando a generar valor a municipios,
+					industrias y la comunidad mediante sistemas de generación de energía a
+					partir de residuos orgánicos. Creemos que la incorporación de nuevas
+					tecnologías a los emprendimientos productivos, especialmente en la
+					industria agroalimentaria, cambiará la manera de hacer negocios en
+					Argentina y la región, adoptando prácticas más eficientes, y a la vez
+					más sustentables. Es por eso que trabajamos junto a productores
+					agropecuarios e industriales para aprovechar un recurso valioso, que
+					hoy está desaprovechado, generando negocios más rentables, con menor
+					riesgo, y amigables con el medio ambiente y la comunidad en la que
+					trabaja.
 				</Text>
 			</Flex>
 		</Flex>
